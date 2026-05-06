344725072c5264d5bb6f986645e6259912813b76
karannath000000-kkn <karannath000000@gmail.com>
2026-05-06 23:39:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let recipes = [];
+
+function addRecipe() {
+    let title = document.getElementById("title").value;
+    let ingredients = document.getElementById("ingredients").value;
+    let steps = document.getElementById("steps").value;
+
+    let recipe = {
+        title,
+        ingredients,
+        steps
+    };
+
+    recipes.push(recipe);
+    displayRecipes();
+}
+
+function displayRecipes() {
+    let list = document.getElementById("recipeList");
+    list.innerHTML = "";
+
+    recipes.forEach((r) => {
+        list.innerHTML += `
+            <div class="recipe">
+                <h3>${r.title}</h3>
+                <p><b>Ingredients:</b> ${r.ingredients}</p>
+                <p><b>Steps:</b> ${r.steps}</p>
+            </div>
+        `;
+    });
+}
