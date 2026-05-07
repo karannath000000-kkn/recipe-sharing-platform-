d5493cc78a2d23e34789478a0a3c249b45b17827
karannath000000-kkn <karannath000000@gmail.com>
2026-05-07 13:02:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,23 @@
-let recipes = [];
+function searchRecipe() {
+    let value = document.getElementById("search").value.toLowerCase();
 
-function addRecipe() {
-    let title = document.getElementById("title").value;
-    let ingredients = document.getElementById("ingredients").value;
-    let steps = document.getElementById("steps").value;
+    let filtered = recipes.filter(r =>
+        r.title.toLowerCase().includes(value)
+    );
 
-    let imageInput = document.getElementById("image");
-    let image = URL.createObjectURL(imageInput.files[0]);
-
-    let recipe = {
-        title,
-        ingredients,
-        steps,
-        image
-        likes:0
-    };
-
-    recipes.push(recipe);
-    displayRecipes();
-
-    document.getElementById("title").value = "";
-    document.getElementById("ingredients").value = "";
-    document.getElementById("steps").value = "";
-    document.getElementById("image").value = "";
-}
-
-function displayRecipes() {
     let list = document.getElementById("recipeList");
     list.innerHTML = "";
 
-    recipes.forEach((r, index) => {
+    filtered.forEach((r, index) => {
         list.innerHTML += `
             <div class="recipe">
                 <img src="${r.image}">
                 <h3>${r.title}</h3>
                 <p><b>Ingredients:</b> ${r.ingredients}</p>
                 <p><b>Steps:</b> ${r.steps}</p>
+
                 <button onclick="likeRecipe(${index})">
-                ❤️ Like (${r.likes})
+                    ❤️ Like (${r.likes})
                 </button>
             </div>
         `;
