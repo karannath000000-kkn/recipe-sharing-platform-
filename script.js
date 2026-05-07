a33971bc6dd42cd8fbefc8d4715e5fe42446ca61
karannath000000-kkn <karannath000000@gmail.com>
2026-05-07 12:00:51 +0530

    Add image handling to recipe submission

--- script.js
+++ script.js
@@ -5,17 +5,23 @@ function addRecipe() {
     let ingredients = document.getElementById("ingredients").value;
     let steps = document.getElementById("steps").value;
 
+    let imageInput = document.getElementById("image");
+    let image = URL.createObjectURL(imageInput.files[0]);
+
     let recipe = {
         title,
         ingredients,
-        steps
+        steps,
+        image
     };
 
     recipes.push(recipe);
     displayRecipes();
+
     document.getElementById("title").value = "";
-document.getElementById("ingredients").value = "";
-document.getElementById("steps").value = "";
+    document.getElementById("ingredients").value = "";
+    document.getElementById("steps").value = "";
+    document.getElementById("image").value = "";
 }
 
 function displayRecipes() {
@@ -25,6 +31,7 @@ function displayRecipes() {
     recipes.forEach((r) => {
         list.innerHTML += `
             <div class="recipe">
+                <img src="${r.image}">
                 <h3>${r.title}</h3>
                 <p><b>Ingredients:</b> ${r.ingredients}</p>
                 <p><b>Steps:</b> ${r.steps}</p>
